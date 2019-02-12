d59791f3e551dd113c26fef6b49dfdc9da729a66
Alejandro <arodriguez1495@gmail.com>
2019-02-12 05:03:28 -0500

    complete almost all methods, printing and importing still missing

--- teacher.py
+++ teacher.py
@@ -1,5 +1,6 @@
 import sqlite3
 import matplotlib.pyplot as plt
+import pandas as pd
 from datetime import datetime
 from matplotlib import style
 style.use("fivethirtyeight")
@@ -17,80 +18,111 @@ class Teacher:
         self.conn = sqlite3.connect('school.db')
         self.c = self.conn.cursor()
 
-    def get_student_info(self, student_id):
-        student_dni = self.c.execute('SELECT dni FROM students WHERE id = {}'.format(student_id)).fetchall()[0]
-        print('Student DNI: ', student_dni)
-        student_subjects = self.c.execute('SELECT id, name FROM subjects WHERE student_id = {}'.format(student_id)).fetchall()
-        print('Student Subjects:\n', student_subjects)
-        return print('------------END-------------')
-
-    def get_statistics(self, group_id, subject_id):
-        grades = pd.read_sql('SELECT * FROM grades WHERE ')
-        ### FUNCTION STILL ON WORK ###
-
-# def get_records(self):
-#     '''The teacher receives the blank minutes of the subjects he is
-#        responsible for.'''
-#
-#     # Read tables from database
-#     subjects = pd.read_sql('SELECT * FROM subjects WHERE teacher_id = "{}"'.format(id), self.conn) # select specific teacher subjects
-#     students = pd.read_sql('SELECT * FROM students', self.conn) # select all students
-#
-#     # Create or open an excel file
-#     writer = pd.ExcelWriter('records_{%d-%m-%Y}.xlsx'.format(datetime.now()), engine='xlsxwriter')
-#
-#     for i, row in subjects.iterrows():
-#         # Report header
-#         row[["course", "campus", "name", "semester", "group_id"]].to_excel(writer, header=False, sheet_name=row['name'])
-#
-#         # Report grades
-#         grades = pd.read_sql('SELECT * FROM grades WHERE subject_id = {}'.format(row['id']), self.conn) # select specific subjects grades
-#
-#         complete_grades = pd.merge(grades, students, how='left', left_on='student_id', right_on='id') # Add student id and dni columns
-#         complete_grades = complete_grades[["id","dni","first_name","last_name","grade"]] # select columns that are going to be in the report
-#         complete_grades.to_excel(writer, index=False, startrow=7, sheet_name=row['name']) # make the report in excel
-#
-#     writer.save() # save changes in the excel file
-#     return 'records exported in records_{%d-%m-%Y}.xlsx file'
-
-    def get_records(self):
+    def get_records(self, grouped=False, export=False):
         '''The teacher receives the blank minutes of the subjects he is
            responsible for.'''
 
+        if export:
+            filename  = '{}-records-{:%d-%m-%Y--%h-%m-%s}.xlsx'.format(self.username,datetime.now())
+            writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+            print('Saved at', filename)
         # Read tables from database
-        subjects = pd.read_sql('SELECT * FROM subjects WHERE teacher_id = "{}"'.format(id), self.conn) # select specific teacher subjects
+        subjects = pd.read_sql('SELECT * FROM subjects WHERE teacher_id = "{}"'.format(self.id), self.conn) # select specific teacher subjects
         students = pd.read_sql('SELECT * FROM students', self.conn) # select all students
 
-        for i, row in subjects.iterrows():
-            # Report subject info
-            print(row[["course", "campus", "name", "semester", "group_id"]])
-            # Report grades
-            grades = pd.read_sql('SELECT * FROM grades WHERE subject_id = {}'.format(row['id']), self.conn) # select specific subjects grades
-            complete_grades = pd.merge(grades, students, how='left', left_on='student_id', right_on='id') # Add student id and dni columns
-            complete_grades = complete_grades[["id","dni","first_name","last_name","grade"]] # select columns that are going to be in the report
-            print(complete_grades)
+        if subjects.shape[0] < 1: print('No subject found')
+        else:
+            for i, row in subjects.iterrows():
+                # Report grades
+                grades = pd.read_sql('SELECT * FROM grades WHERE subject_id = {}'.format(row['id']), self.conn) # select specific subjects grades
+                complete_grades = pd.merge(grades, students, how='left', left_on='student_id', right_on='id') # Add student id and dni columns
+                complete_grades = complete_grades[["id","dni","first_name","last_name","grade"]] # select columns that are going to be in the report
 
+                if export:
+                    row[["course", "campus", "name", "semester", "group_id"]].to_excel(writer, header=False, sheet_name=row['name']) # Report subject info
+                    complete_grades.to_excel(writer, index=False, startrow=7, sheet_name=row['name']) # make the report in excel
+                else:
+                    print('Subject Information:\n')
+                    print(row[["course", "campus", "name", "semester", "group_id"]]) # Report subject info
+                    print()
+                    print('Grades:\n')
+                    print(complete_grades)
+                    print()
+
+        if export: writer.save()
+
+        return print('records printed')
+
+    def set_grades(self, subject_id, student_id, grade):
+        sql = ''' UPDATE grades
+                  SET grade = ?
+                  WHERE subject_id = ?
+                  AND student_id = ?'''
+
+        self.c.execute(sql, (grade, subject_id, student_id))
+        return print('grade updated')
+
+    def add_student(self, subject_id, student_id):
+        sql = '''INSERT INTO grades(student_id, subject_id)
+                 VALUES (?, ?)'''
+        self.c.execute(sql, (student_id, subject_id))
+        return print('student added to subject')
+
+    def remove_student(self, subject_id, student_id):
+        sql = ''' DELETE FROM grades
+                  WHERE subject_id = ?
+                  AND student_id = ? '''
+        self.c.execute(sql, (subject_id, student_id))
+        return print('student removed from record')
+
+    def import_records(self, path_to_file, grouped=False):
+        header = pd.read_excel(path_to_file, header=None, index_col=0, usecols=[0,1], nrows=5, skiprows=0).T
+        self.c.execute('SELECT id FROM subjects WHERE name = "{}"'.format(header.iloc[0,2]))
+        subject_id = self.c.fetchall()[0][0]
+
+        grades = pd.read_excel(path_to_file, skiprows=7)
+        grades['subject_id'] = subject_id
+        grades.to_sql('temp_grades', self.conn, if_exists='replace')
+
+        sql = ''' UPDATE grades
+                  SET grade = temp_grades.grade
+                  FROM temp_grades
+                  WHERE subject_id = temp_grades.subject_id
+                  AND student_id = t.id'''
+
+        self.c.execute(sql)
+        return print('grades updated via file')
+
+    def print_records(self, grouped=False):
+        ### STILL ON WORK ###
         return
 
-    def get_records():
-        return
+    def get_student_info(self, student_id):
+        student_dni = self.c.execute('SELECT dni FROM students WHERE id = {}'.format(student_id)).fetchall()[0][0]
+        print('Student DNI: ', student_dni, end='\n')
 
-    def set_grades():
-        return
-    def add_student():
-        return
+        g = pd.read_sql('SELECT * FROM grades', self.conn)
+        s = pd.read_sql('SELECT * FROM subjects', self.conn)
+        merged = pd.merge(g, s, how='left', left_on='subject_id', right_on='id')
+        student_subjects = merged[merged['student_id']==1][['id','name']].drop_duplicates()
+        print('List of subjects: ')
+        print('Subject      Subject\n', student_subjects.to_string(index=False))
 
-    def remove_student():
-        return
+        return print('Student information printed')
 
-    def export_records():
-        return
+    def get_statistics(self, group_id, subject_id):
+        grades = pd.read_sql('SELECT * FROM grades WHERE subject_id = {}'.format(subject_id), self.conn)
 
-    def import_records():
-        return
+        first_stats = pd.DataFrame()
+        first_stats['Number'] = grades['grade'].value_counts()
+        first_stats['% over presents'] = grades['grade'].value_counts() / grades[~grades['grade'].isna()].shape[0]
+        first_stats['% over total'] = grades['grade'].value_counts() / grades[~grades['grade'].isna()].shape[0]
+        print(first_stats)
 
-    def print_records():
-        return
+        print('Promedio: ', grades['grade'].mean())
 
-a = Teacher(id=2423, username='rer', password='re', first_name='rere', last_name='rere')
-type(a) == Teacher
+        ranges = list(range(0, 21, 2))
+        grades.groupby(pd.cut(grades['grade'], ranges)).count().plot.bar(y='grade')
+        plt.show()
+
+        return print('statistics showed')
